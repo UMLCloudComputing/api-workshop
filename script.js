1eeed16e6646ddce032448a9b30c7d2ae9822bfb
cjcocokrisp <chriscoco1205@gmail.com>
2025-03-09 15:45:16 -0400

    feat: add visualization for the demo that shows off the state of the database as well as the transactions

---
+++ script.js
@@ -0,0 +1,47 @@
+(async () => {
+    try {
+        let res = await fetch('http://localhost:5000/database');
+        let data = await res.json();
+            let dbContainer = document.getElementById('db-visualization');
+            data.items.forEach(element => {
+                let divUsername = document.createElement('div');
+                divUsername.innerText = element.username;
+                dbContainer.appendChild(divUsername);
+        
+                let divEmail = document.createElement('div');
+                divEmail.innerText = element.email;
+                dbContainer.appendChild(divEmail);
+        
+                let divAge = document.createElement('div');
+                divAge.innerText = element.age;
+                dbContainer.appendChild(divAge);
+        
+                let divGpa = document.createElement('div');
+                divGpa.innerText = element.gpa;
+                dbContainer.appendChild(divGpa);
+            });
+        
+            let transactionContainer = document.getElementById('transactions');
+            data.transactions.forEach(element => {
+                let div = document.createElement('div');
+                div.innerText = element;
+                transactionContainer.appendChild(div);
+            });
+    } catch { 
+        document.body.innerHTML = '';
+        let div = document.createElement('div')
+        div.id = 'container'
+
+        let errorText = document.createElement('div');
+        errorText.id = 'error-text'
+        errorText.innerText = 'An error has occured or the server is not running!\n \
+                               Check if the server has started and once started refresh the page.\n \
+                               If you are still getting an error please report it to the demo\'s repository.'
+        div.appendChild(errorText)
+        
+        document.body.appendChild(div);
+    }   
+
+    let button = document.getElementById('refresh-button');
+    button.addEventListener('click', () => { location.reload() });
+})();
--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import Flask, request
+from flask_cors import CORS
+from datetime import datetime
 import json
 
 app = Flask('api-workshop')
+CORS(app)
 
 mock_db = {
     'johndoe57': {
@@ -11,12 +14,16 @@ mock_db = {
     }
 }
 
+transactions = []
+
 @app.route('/users', methods=['GET', 'POST', 'PATCH', 'DELETE'])
 def users_interaction():
     username = request.args.get('username')
     email = request.args.get('email')
     age = request.args.get('age')
     gpa = request.args.get('gpa')
+
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     status_code = None
     if request.method == 'GET':
@@ -31,6 +38,9 @@ def users_interaction():
             status_code = 200
         else:
             status_code = 404
+
+        transactions.append(f'GET for {username} @ {time}, Status: {status_code}')
+
         return app.response_class(
             response=json.dumps(data),
             status=status_code,
@@ -46,6 +56,9 @@ def users_interaction():
             status_code = 201
         else:
             status_code = 500
+
+        transactions.append(f'POST creating {username} @ {time}, Status: {status_code}')
+
         return app.response_class(status=status_code)
     elif request.method == 'PATCH':
         new_data = {
@@ -57,6 +70,9 @@ def users_interaction():
             if new_data[key] != None:
                 mock_db[username][key] = new_data[key]
         status_code = 200
+
+        transactions.append(f'PATCH on {username} @ {time}, Status: {status_code}')
+
         return app.response_class(status=200)
     elif request.method == 'DELETE':
         if username in mock_db.keys():
@@ -64,4 +80,25 @@ def users_interaction():
             status_code = 200
         else:
             status_code = 500
+
+        transactions.append(f'DELETE on {username} @ {time}, Status: {status_code}')
+
         return app.response_class(status=status_code)
+    
+@app.route('/database', methods=['GET'])
+def database_interaction():
+    data = [{
+        'username': key,
+        'email': mock_db[key]['email'],
+        'age': mock_db[key]['age'],
+        'gpa': mock_db[key]['gpa']
+    } for key in mock_db.keys()]
+
+    transactions_copy = transactions
+    transactions_copy.reverse()
+
+    return app.response_class(
+        response=json.dumps({'items': data, 'transactions': transactions_copy}),
+        status=200,
+        mimetype='application/json'
+    )
